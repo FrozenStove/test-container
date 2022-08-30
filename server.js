94c2f8acc5f38a9ae8b0d673bc235c28202e427d
FrozenStove <Yessysutter@gmail.com>
2022-08-29 21:32:28 -0700

    teapot

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const PORT = 3000;
 
 app.use(express.static(path.resolve(__dirname, './')));
 
+app.get('/overload', (req, res) => {
+    const a = [];
+    for ( let i = 0; i < 10000000000000000000; i++){
+        a.push(i)
+    }
+    return res.sendStatus(204)
+});
+
+app.get('/teapot', (req, res) => {
+    console.log('teapot')
+    return res.sendStatus(418)
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}...`);
